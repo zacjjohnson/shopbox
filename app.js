3452598b11b7e6640527eddcbf4f823838f96fe6
zacjjohnson <wecooked@gmail.com>
2022-10-22 14:46:16 -0400

    update

--- app.js
+++ app.js
@@ -33,14 +33,7 @@ app.use((req, res, next) => {
   next();
 })
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://shopboxuser:${process.env.MONGOPASSWORD}@shopboxcluster.kuyuc8m.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
 
 // 👇 Start handling routes here
 const index = require("./routes/index.routes");
